3dd3cd5e2c5fd3de503e3a621e87c2f07a814552
Emmanuel <olajideemmanuel250@gmail.com>
2025-09-02 19:52:25 +0100

    feat: changed layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import { BookOpen } from 'lucide-react';
+import logo from "/public/Logo_Black_Edited-removebg-preview.png"
 
 export const Footer = () => {
   return (
@@ -7,7 +8,7 @@ export const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
-              <BookOpen className="h-6 w-6 text-primary" />
+              {/* <BookOpen className="h-6 w-6 text-primary" /> */}
               <span className="text-lg font-semibold">IJSDS</span>
             </div>
             <p className="text-sm text-muted-foreground">
