281ccea7bbb4a857c6c3dee7ada13a03c0e5dc19
Emmanuel <olajideemmanuel250@gmail.com>
2025-09-06 16:02:57 +0100

    feat:updated paystack function

--- src/components/paystack/paystackFunction.tsx
+++ src/components/paystack/paystackFunction.tsx
@@ -1,5 +1,7 @@
 import { PaystackButton } from "react-paystack"
-const publicKey = "pk_test_7d354e4f3e5488bdb829c2a655e4e731f6263ea9"
+// const publicKey = "pk_test_7d354e4f3e5488bdb829c2a655e4e731f6263ea9"
+const publicLiveKey = process.env.VITE_PAYSTACK_PUBLIC_KEY
+let publicKey = publicLiveKey
 const Paystackbtn = ({info}) => {
     const config = {
         email:info.email,
