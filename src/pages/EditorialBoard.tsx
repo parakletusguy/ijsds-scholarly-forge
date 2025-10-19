0bce6ec11b699592571f2520714fed10fdde0fbb
Emmanuel <olajideemmanuel250@gmail.com>
2025-10-19 20:03:39 +0100

    edited board of directors

--- src/pages/EditorialBoard.tsx
+++ src/pages/EditorialBoard.tsx
@@ -7,17 +7,115 @@ import { ArrowLeft } from "lucide-react";
 
 const EditorialBoard = () => {
   const boardMembers = [
-    { name: "Dr. Mina Margaret Ogbanga", role: "Editor-in-Chief", isChief: true },
-    { name: "Professor Okon Godwin", role: "Editorial Board Member" },
-    { name: "Professor Margaret Akpomi", role: "Editorial Board Member" },
-    { name: "Professor Margaret Adamek", role: "Editorial Board Member" },
-    { name: "Professor Shashikant Nishant Sharma", role: "Editorial Board Member" },
-    { name: "Professor Godspower J. Irikana", role: "Editorial Board Member" },
-    // { name: "Professor Uzoma Okoye", role: "Editorial Board Member" },
-    // { name: "Professor Onyeama", role: "Editorial Board Member" },
-    { name: "Professor Uranta Daniel", role: "Editorial Board Member" },
-    { name: "Professor T. Adolphus", role: "Editorial Board Member" },
-  ];
+  { 
+    name: "Dr. Mina Ogbanga", 
+    role: "Editor-in-Chief", 
+    isChief: true,
+    Designation: "Editor-in-Chief, IJSDS & Associate Professor of Social Work",
+    Institution: "Department of Social Work, River State University",
+    Contact: "mina.ogbanga@ust.edu.ng",
+    Education: "",
+    Expertise: "",
+    ResearchInterests: "",
+    Location: ""
+  },
+  { 
+    name: "Professor Roberto Ariel Abeldaño Zuñiga", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Full Professor & University Lecturer",
+    Institution: "University of Sierra Sur, Mexico & University of Helsinki, Finland",
+    Contact: "",
+    Education: "Ph.D. in Demography (National University of Córdoba, Argentina)",
+    Expertise: "Demography",
+    ResearchInterests: "Public health and environment; disasters, mental health and vulnerable populations; public health in disaster situations.",
+    Location: ""
+  },
+  { 
+    name: "Professor Ukamaka M. Oruche", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Gordon Keller Professor of Nursing; Senior Associate Dean of Research and Director of the PhD Program",
+    Institution: "USF Health College of Nursing",
+    Contact: "orucheu@usf.edu | O: 813-396-2524",
+    Education: "PhD, RN, PMHCNS-BC, FAAN",
+    Expertise: "Nursing Research, PMHCNS-BC, FAAN",
+    ResearchInterests: "",
+    Location: "12901 Bruce B. Downs Blvd., MDC 22 | Tampa, FL | 33612"
+  },
+  { 
+    name: "Professor Olufemi Martins Adesope", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Professor of Agricultural Extension and ICT in Agriculture",
+    Institution: "Department of Agricultural Extension and Development Studies, University of Port Harcourt, Nigeria",
+    Contact: "",
+    Education: "",
+    Expertise: "Data Analyst (Quantitative & Qualitative), ICT integration, Agricultural Extension, Rural Sociology, Research Methods.",
+    ResearchInterests: "",
+    Location: ""
+  },
+  { 
+    name: "Dr. Khadija Khaja", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Associate Professor",
+    Institution: "School of Social Work, Indiana University, Indianapolis",
+    Contact: "kkhaja@iu.edu | 317-278-8609",
+    Education: "MSW, PhD",
+    Expertise: "Social Work",
+    ResearchInterests: "",
+    Location: ""
+  },
+  { 
+    name: "Professor T. Adolphus", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Professor",
+    Institution: "Department of Science Education, Faculty of Education, Rivers State University",
+    Contact: "",
+    Education: "",
+    Expertise: "Science Education",
+    ResearchInterests: "",
+    Location: ""
+  },
+  { 
+    name: "Professor Daniel Uranta", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Professor of Social Work and Community Development",
+    Institution: "University of Port-Harcourt, Rivers State",
+    Contact: "",
+    Education: "",
+    Expertise: "Social Work and Community Development",
+    ResearchInterests: "",
+    Location: ""
+  },
+  { 
+    name: "Professor Godspower Jackson Irikana", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Professor",
+    Institution: "Ignatius Ajuru University of Education (IAUE)",
+    Contact: "",
+    Education: "Ph.D. in Sociology of Development (2007); M.Sc. in Sociology of Development (2002); B.A. in Political Science and Education (1988)",
+    Expertise: "Sociology of Development; External Examiner/Assessor; NUC/NCCE accreditation team member; Consultant (RSSDA, CASS, OSIWA, INEC).",
+    ResearchInterests: "",
+    Location: ""
+  },
+  { 
+    name: "Shashikant Nishant Sharma", 
+    role: "Editorial Board Member", 
+    isChief: false,
+    Designation: "Research Head",
+    Institution: "Department of Architecture and Planning, Maulana Azad National Institute of Technology (Bhopal, India) & Track2Training (New Delhi, India)",
+    Contact: "urp2025@gmail.com",
+    Education: "",
+    Expertise: "Research",
+    ResearchInterests: "",
+    Location: ""
+  }
+];
   const navigate = useNavigate()
 
   return (
@@ -65,6 +163,22 @@ const EditorialBoard = () => {
                     <CardTitle className="text-xl">{member.name}</CardTitle>
                     <CardTitle className="text-sm"><a href="https://minaogbanga.com">www.minaogbanga.com </a></CardTitle>
                     <Badge variant="default" className="mx-auto">{member.role}</Badge>
+                  <CardContent className="gap-3">
+{member.Designation && <p className="text-[12px] py-1" >Designation: <span className="font-bold">{member.Designation}</span> </p>
+}
+{member.Institution && <p className="text-[12px] py-1" >Institution: <span className="font-bold">{member.Institution}</span> </p>
+}
+{member.Contact && <p className="text-[12px] py-1" >Contact: <span className="font-bold">{member.Contact}</span> </p>
+}
+{member.Education && <p className="text-[12px] py-1" >Education: <span className="font-bold">{member.Education}</span> </p>
+}
+{member.Expertise && <p className="text-[12px] py-1" >Expertise: <span className="font-bold">{member.Expertise}</span> </p>
+}
+{member.ResearchInterests && <p className="text-[12px] py-1" >Research Interests: <span className="font-bold">{member.ResearchInterests}</span> </p>
+}
+{member.Location && <p className="text-[12px] py-1" >Location: <span className="font-bold">{member.Location}</span> </p>
+}
+                    </CardContent>
                   </CardHeader>
                 </Card>
               ))}
@@ -73,7 +187,7 @@ const EditorialBoard = () => {
           {/* Editorial Board Members */}
           <div>
             <h2 className="text-2xl font-semibold mb-8 text-center">Editorial Board Members</h2>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-6">
               {boardMembers
                 .filter(member => !member.isChief)
                 .map((member, index) => (
@@ -82,6 +196,22 @@ const EditorialBoard = () => {
                       <CardTitle className="text-lg">{member.name}</CardTitle>
                       <Badge variant="secondary" className="mx-auto">{member.role}</Badge>
                     </CardHeader>
+                    <CardContent className="gap-3">
+{member.Designation && <p className="text-[12px] py-1" >Designation: <span className="font-bold">{member.Designation}</span> </p>
+}
+{member.Institution && <p className="text-[12px] py-1" >Institution: <span className="font-bold">{member.Institution}</span> </p>
+}
+{member.Contact && <p className="text-[12px] py-1" >Contact: <span className="font-bold">{member.Contact}</span> </p>
+}
+{member.Education && <p className="text-[12px] py-1" >Education: <span className="font-bold">{member.Education}</span> </p>
+}
+{member.Expertise && <p className="text-[12px] py-1" >Expertise: <span className="font-bold">{member.Expertise}</span> </p>
+}
+{member.ResearchInterests && <p className="text-[12px] py-1" >Research Interests: <span className="font-bold">{member.ResearchInterests}</span> </p>
+}
+{member.Location && <p className="text-[12px] py-1" >Location: <span className="font-bold">{member.Location}</span> </p>
+}
+                    </CardContent>
                   </Card>
                 ))}
             </div>
